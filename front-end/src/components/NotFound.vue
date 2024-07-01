49b27dd56ccbbe58d07ee0e2aa89eb83a1fb1aa7
mnew2m <mnew2m@gmail.com>
2024-07-01 22:59:33 +0900

    404페이지 디자인

--- front-end/src/components/NotFound.vue
+++ front-end/src/components/NotFound.vue
@@ -1,33 +1,19 @@
 <template>
-  <v-app>
-    <v-main>
-      <v-container>
-        <v-row>
-          <v-col>
-            존재하지 않는 페이지 디자인하기
-          </v-col>
-        </v-row>
-      </v-container>
-    </v-main>
-  </v-app>
+  <div class="d-flex justify-center text-left">
+    <v-card max-width="344" color="primary" variant="tonal">
+      <v-card-item>
+        <div>
+          <div class="text-overline mb-1">| ALERT |</div>
+          <div class="text-h6 mb-1">404</div>
+          <div class="text-caption">존재하지 않는 페이지이거나 오류가 발생하였습니다.</div>
+        </div>
+      </v-card-item>
+      <v-card-actions class="d-flex-center">
+        <v-btn :to="{ path: '/test' }"> 돌아가기 </v-btn>
+      </v-card-actions>
+    </v-card>
+  </div>
 </template>
 
 <script setup>
-
 </script>
-
-<style scoped>
-.not-found {
-  line-height: 1.2;
-  text-align: center;
-  font-family: "Oswald", sans-serif;
-  padding: 80px 20px;
-  .status {
-    font-size: 160px;
-    color: $primary;
-  }
-  .message {
-    font-size: 50px;
-  }
-}
-</style>
