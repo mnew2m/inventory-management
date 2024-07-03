8f907868d8be92e31d776e91d8088d3a2df19fef
mnew2m <mnew2m@gmail.com>
2024-07-03 23:49:06 +0900

    - pinia 추가

--- front-end/src/main.js
+++ front-end/src/main.js
@@ -1,9 +1,13 @@
 import {createApp} from "vue";
+import {createPinia} from "pinia";
 import App from "@/App.vue";
 import router from "@/router/router.js";
 import vuetify from "@/plugins/vuetify.js";
 
+const pinia = createPinia()
+
 createApp(App)
-    .use(router)
     .use(vuetify)
+    .use(router)
+    .use(pinia)
     .mount('#app');
---
+++ front-end/src/stores/index.js
@@ -0,0 +1,14 @@
+import {defineStore} from "pinia";
+
+export const useInventoryStore = defineStore('inventory', {
+    state: () => {
+        return { count: 0 }
+    },
+    // could also be defined as
+    // state: () => ({ count: 0 })
+    actions: {
+        increment() {
+            this.count++
+        },
+    },
+})
