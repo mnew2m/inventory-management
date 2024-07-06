f6115bc1edec76b9d2c9393dd1acb90c62d032fe
mnew2m <mnew2m@gmail.com>
2024-07-06 11:59:00 +0900

    - header menu link 연결, avatar menu 컴포넌트화

---
+++ front-end/src/components/common/HeaderAvatarMenu.vue
@@ -0,0 +1,36 @@
+<template>
+  <v-menu min-width="200px" rounded>
+    <template #activator="{ props }">
+      <v-btn icon="true" v-bind="props">
+        <v-avatar icon="mdi-account-circle" />
+      </v-btn>
+    </template>
+    <v-card>
+      <v-card-text>
+        <div class="mx-auto text-center">
+          <v-btn :variant="props.variant" rounded>
+            EDIT ACCOUNT
+          </v-btn>
+          <v-divider class="my-2"></v-divider>
+          <v-btn :variant="props.variant" rounded>
+            CHANGE PASSWORD
+          </v-btn>
+          <v-divider class="my-2"></v-divider>
+          <v-btn :variant="props.variant" rounded>
+            LOG OUT
+          </v-btn>
+        </div>
+      </v-card-text>
+    </v-card>
+  </v-menu>
+</template>
+
+<script setup>
+const props = defineProps({
+  variant: {
+    default: 'plain'
+  }
+});
+</script>
+
+<style scoped></style>
--- front-end/src/components/common/TheHeader.vue
+++ front-end/src/components/common/TheHeader.vue
@@ -1,23 +1,30 @@
 <template>
   <v-app-bar class="px-3" density="compact" flat>
-    <v-avatar class="hidden-md-and-up" color="grey-darken-1" size="32"/>
+    <div class="hidden-md-and-up">
+      <HeaderMenu />
+    </div>
     <v-spacer/>
     <v-tabs color="grey-darken-2" centered>
-      <v-tab v-for="link in links" :key="link">
-        {{link}}
+      <v-tab v-for="page in pages" :key="page.name" :to="page.path">
+        {{ page.name }}
       </v-tab>
     </v-tabs>
     <v-spacer/>
-    <v-avatar class="hidden-sm-and-down" color="grey-darken-1" size="32"/>
+    <!--<v-avatar class="hidden-sm-and-down" icon="mdi-account-circle" />-->
+    <div class="hidden-sm-and-down">
+      <HeaderMenu />
+    </div>
   </v-app-bar>
 </template>
 
 <script setup>
-const links = [
-  'Dashboard',
-  'Messages',
-  'Profile',
-  'Updates',
+import HeaderMenu from "@/components/common/HeaderAvatarMenu.vue";
+
+const pages = [
+  { name: 'HOME', path: '/' },
+  { name: 'PURCHASE HISTORY', path: '/purchaseHistory' },
+  { name: 'REPLACEMENT CYCLES', path: '/replacementCycles' },
+  { name: 'SYSTEM', path: '/system' },
 ]
 </script>
 
