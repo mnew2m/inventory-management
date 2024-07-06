9e35e7761360a2c5c07d27387b1aa00effca9ec7
mnew2m <mnew2m@gmail.com>
2024-07-06 16:32:05 +0900

    - 구매내역관리 페이지 테이블/검색필드 적용

--- front-end/src/pages/PurchaseHistoryPage.vue
+++ front-end/src/pages/PurchaseHistoryPage.vue
@@ -1,26 +1,69 @@
 <template>
   <div class="d-flex justify-center">
     <v-row justify="center">
-      <v-col cols="12" md="8">
-        <v-sheet class="bg-grey-lighten-3" min-height="85vh" rounded="lg">
-          <v-btn color="error" @click="plusCounter">Purchase History Page</v-btn>
-          <div>Current Count: {{ counter.count }}</div>
-        </v-sheet>
+      <v-col cols="12" md="9">
+        <v-text-field
+            v-model="search"
+            placeholder="Search"
+            prepend-inner-icon="mdi-magnify"
+            variant="solo-filled"
+            density="compact"
+            flat
+            hide-details
+            single-line
+            width="300px"
+            clearable
+            persistent-clear
+        ></v-text-field>
+        <v-data-table
+          :headers="gridHeaders"
+          :items="gridData"
+          item-key="purchasePlace"
+          :search="search"
+          :disable-sort="true"
+          hide-default-footer
+        ></v-data-table>
       </v-col>
     </v-row>
   </div>
 </template>
 
 <script setup>
-import {useInventoryStore} from "@/stores/index.js";
+import {ref} from "vue";
 
-const counter = useInventoryStore()
+const search = ref('');
 
-const plusCounter = () => {
-  counter.count++
-  // with autocompletion ✨
-  counter.$patch({ count: counter.count + 1 })
-  // or using an action instead
-  counter.increment()
-}
+const gridHeaders = [
+  { title: '구매처', key: 'purchasePlace' },
+  { title: '이미지', key: 'purchaseImage' },
+  { title: '카테고리', key: 'category' },
+  { title: '상품명', key: 'productName' },
+  { title: '메모', key: 'productMemo' },
+  { title: '가격', key: 'productPrice', value: item => `${item.productPrice}`.toLocaleString() },
+  { title: '수량', key: 'productQty' },
+  { title: '구매일', key: 'purchaseDate' },
+];
+
+const gridData = [
+  {
+    purchasePlace: '쿠팡',
+    purchaseImage: '이미지 url',
+    category: '생활용품',
+    productName: '휴대폰충전기',
+    productMemo: '1미터짜리로 삼',
+    productPrice: 5000,
+    productQty: 1,
+    purchaseDate: '2024-07-02',
+  },
+  {
+    purchasePlace: '11번가',
+    purchaseImage: '이미지 url',
+    category: '생활용품',
+    productName: '로지텍마우스',
+    productMemo: 'G304',
+    productPrice: 45000,
+    productQty: 1,
+    purchaseDate: '2024-07-04',
+  }
+];
 </script>
