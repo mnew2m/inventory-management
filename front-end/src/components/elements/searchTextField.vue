fa57f4f1626c2cae0ddd8892f5f2c70fbae9ad71
ChoiMyungShin <coehoan@learnershi.com>
2024-07-09 18:18:50 +0900

    - searchTextField 컴포넌트 생성

---
+++ front-end/src/components/elements/searchTextField.vue
@@ -0,0 +1,44 @@
+<template>
+  <v-text-field
+      :v-model="searchModel"
+      prepend-inner-icon="mdi-magnify"
+      placeholder="검색"
+      variant="solo-filled"
+      density="compact"
+      flat
+      :max-width="maxWidth"
+      rounded="0"
+      hide-details
+      clearable
+      persistent-clear
+      @update:model-value="onChangeHandler"
+  ></v-text-field>
+</template>
+
+<script setup>
+import {toRefs} from "vue";
+
+const emit =defineEmits(['onUpdate']);
+
+const props = defineProps({
+  searchModel: {
+    required: true
+  },
+  maxWidth: {
+    default: 300
+  },
+});
+
+const {
+  searchModel,
+  maxWidth,
+} = toRefs(props);
+
+function onChangeHandler(val) {
+  emit('onUpdate', val);
+}
+</script>
+
+<style scoped>
+
+</style>
