9518619de7c253eebd5af96838984d0ce9160243
mnew2m <mnew2m@gmail.com>
2024-07-14 17:07:07 +0900

    - grid text field component

---
+++ front-end/src/components/elements/gridTextField.vue
@@ -0,0 +1,33 @@
+<template>
+  <v-text-field
+    class="text-subtitle-2"
+    variant="solo"
+    density="compact"
+    flat
+    tile
+    hide-details
+    :model-value="props.modelVal"
+    :readonly="props.readonly"
+    min-width="100" />
+</template>
+
+<script setup>
+const props = defineProps({
+  modelVal: {
+    default: '',
+  },
+  readonly: {
+    type: Boolean,
+    default: false,
+  }
+});
+</script>
+
+<style scoped>
+.text-field-font {
+  font-size: 0.875rem !important;
+  font-weight: 400;
+  line-height: 1.425;
+  letter-spacing: 0.0178571429em;
+}
+</style>
